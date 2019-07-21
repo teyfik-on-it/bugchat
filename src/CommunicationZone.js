98815af7f007692fc0c96ddaef8330c84fedc16d
Alex Langer <aclanger131@gmail.com>
2019-07-21 19:58:59 +0200

    include advanced dialogues

--- src/CommunicationZone.js
+++ src/CommunicationZone.js
@@ -10,6 +10,7 @@ class CommunicationZone extends Component {
         super(props);
         this.state = {
             value : '',
+            disposable : '',
             history : ["How can I help?"],
         }
 
@@ -29,6 +30,7 @@ class CommunicationZone extends Component {
         if (event.key === 'Enter') {
             this.setState({
                 value: '',
+                disposable : event.target.value,
                 history : [...this.state.history, event.target.value]
             });
 
@@ -40,13 +42,29 @@ class CommunicationZone extends Component {
       }
 
       dialogueEngine(){
-        const answers = ["can you elaborate?","and why do you believe that is so?","can you be more specific?","what would be your guess?"]; 
-        let response = answers[Math.floor(Math.random()*answers.length)];
-        console.log(response);
+        const answersBasic = ["can you elaborate?","and why do you believe that is so?","can you be more specific?","what would be your guess?","I need more details for this one"]; 
+        const answersAdvanced = ["have you check the logs?","have you tried restarting?","what does the documentation say?", "Maybe its a typo"]
+        const answersAdjust = ["you need to be a bit more specific","come on I am trying to help","whatever","that does not sound like a bug"]
         
+        if (this.state.disposable.length <= 7) {
+            let response = answersAdjust[Math.floor(Math.random()*answersAdjust.length)];
             this.setState({
                 history : [...this.state.history, response]
             });
+        } else if (this.state.history.length <= 3 && this.state.value.length > 2) {
+            let response = answersBasic[Math.floor(Math.random()*answersBasic.length)];
+            this.setState({
+                history : [...this.state.history, response]
+            });
+        } else if (this.state.history.length >= 4) {
+            let response = answersAdvanced[Math.floor(Math.random()*answersAdvanced.length)];
+            this.setState({
+                history : [...this.state.history, response]
+            });
+
+
+        }
+            
         }
         
 
